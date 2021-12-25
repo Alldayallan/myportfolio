904f1322b701db50b81b4cd896bba81f2b8615b3
Allan <mail@allanreid.co>
2021-12-24 23:18:43 -0800

    pie charts added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,46 @@ $(document).ready(function() {
     $('#slides').superslides({
         animation: 'fade',
         play: 5000,
-        pagination: false,
-        inherit_width_from: window
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Technical Recruitment", "Software Developer", "Network Novitiate"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#ecf0f1',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
     });
 
 });
